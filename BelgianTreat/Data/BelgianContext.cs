88fd7aaa975a9734aabfe5c7f48be7704b825479
Carpentier Quentin <quentcarp@hotmail.com>
2017-11-05 14:36:18 +0100

    Configuration for the db properties + migration

---
+++ BelgianTreat/Data/BelgianContext.cs
@@ -0,0 +1,21 @@
+﻿using BelgianTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BelgianTreat.Data
+{
+    public class BelgianContext : DbContext
+    {
+        public BelgianContext(DbContextOptions<BelgianContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
--- BelgianTreat/Startup.cs
+++ BelgianTreat/Startup.cs
@@ -1,21 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using BelgianTreat.Services;
+using BelgianTreat.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace BelgianTreat
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BelgianContext>(cfg =>
+            {
+                cfg.UseSqlServer(_config.GetConnectionString("BelgianConnectionString"));
+            });
+
             services.AddTransient<IMailService, NullMailService>();
             // Support for real mail service 
             // TODO
